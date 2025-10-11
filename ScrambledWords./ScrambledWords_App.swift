1abc98fdcd489b81b3a9df54754e5aa8da4c18f6
SohaElGaly <sohaelgalyy@gmail.com>
2025-10-11 14:05:42 +0300

    Initial Commit

---
+++ ScrambledWords./ScrambledWords_App.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambledWords_App.swift
+//  ScrambledWords.
+//
+//  Created by Soha Elgaly on 11/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrambledWords_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
